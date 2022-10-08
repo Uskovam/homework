fc0d01b2ccd5c2c788362f8fa86bb969365490c0
Uskova <uscovam@gmail.com>
2022-10-08 13:18:20 +0300

    задача 4 нахождение максимаьного числа из 3

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 22;
+int b1 = 3;
+int c1 = 9;
+
+
+int max = Max(a1, b1, c1);
+
+Console.WriteLine(max);
