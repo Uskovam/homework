c3616e5aef19c0b2960f2eb357f5974da3f485a3
Uskova <uscovam@gmail.com>
2022-10-08 13:41:15 +0300

    задача 8

---
+++ task8/Program.cs
@@ -0,0 +1,26 @@
+﻿
+void FullArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index > length)
+    {
+        collection = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position > count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int[] array = new int[10];
+
+FullArray(array);
+PrintArray(array);
+Console.WriteLine();
